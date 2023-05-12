3d05596a0af7f4c5c90eea09e0b24b1bc9774e72
leejason28 <jason928lee@gmail.com>
2023-05-12 16:16:31 -0700

    need to piece together logic for save/load

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # odin-hangman
 
 odin project: hangman. simple hangman game to be run on the command line. added functionality to allow player to save the game and load into it the next time the game is run
+
+to run: use command 'ruby lib/hangman.rb' in the odin-hangman folder
--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class Game
 
   @@words = File.readlines('google-10000-english-no-swears.txt')
@@ -9,7 +11,11 @@ class Game
     end
   end
 
-  attr_accessor :secret, :secret_array, :guess_array
+  attr_accessor :secret, :secret_array, :guess_array, :guesses, :user_guessed, :user_incorrect_guessed
+
+  def self.secret_words
+    @@secret_words
+  end
 
   def initialize
     @secret = @@secret_words[Random.rand(@@secret_words.length)]
@@ -85,8 +91,54 @@ class Game
     end
   end
 
+  def to_json
+    JSON.dump ({
+      :secret => @secret,
+      :secret_array => @secret_array,
+      :guess_array => @guess_array,
+      :guesses => @guesses,
+      :user_guessed => @user_guessed,
+      :user_incorrect_guessed => @user_incorrect_guessed
+    })
+  end
+
+  def from_json(string)
+    JSON.load string
+  end
+
+  def save(string)
+    #write to new file with contents of json dump
+    File.open("#{string}.json", 'w') do |file|
+      file.puts self.to_json
+    end
+  end
+
+  def load(string)
+    #read specified file and update instance variables with info from json 
+    #delete the file
+    data = to_json
+    File.open("#{string}.json", 'r') do |file|
+      data = from_json(file)
+    end
+    File.delete("#{string}.json")
+    @secret = data['secret']
+    @secret_array = data['secret_array']
+    @guess_array = data['guess_array']
+    @guesses = data['guesses']
+    @user_guessed = data['user_guessed']
+    @user_incorrect_guessed = data['user_incorrect_guessed']
+  end
+
 end
 
 g = Game.new
-p g.secret
+#File.open("#{g.secret}.json", 'w') do |file|
+  #file.puts g.to_json
+#end
+#gd = 'apple'
+#File.open("#{g.secret}.json", 'r') do |file|
+  #gd = g.from_json(file)
+#end
+#p gd
+#File.delete("#{g.secret}.json")
 g.play

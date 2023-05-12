704b0e574c1267c5751ec0ce85db584985859a74
leejason28 <jason928lee@gmail.com>
2023-05-12 14:02:53 -0700

    need to check for valid/unique guesses, declare win or lose.

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -18,16 +18,37 @@ class Game
     for i in 0...@secret_array.length
       @guess_array.push("_")
     end
+    @guesses = 6 #head,body,2arms,2legs
   end
 
   def play
-    puts "Hello, welcome to Hangman."
+    puts "Hello, welcome to Hangman. Guess the secret word one letter at a time."
+    puts "Correct guesses will give you hints on the secret word. You have a total of 6 incorrect guesses."
+    while !self.game_over?
+      puts @guess_array.join(" ")
+      puts "Guess a letter. You have #{@guesses} incorrect guesses remaining."
+      user_input = gets
+      user_guess = user_input.gsub("\n", "")
+      if @secret_array.include?(user_guess)
+        @guess_array.map!.with_index { |letter, index| @secret_array[index]==user_guess ? user_guess : letter }
+      else
+        @guesses -= 1
+      end
+    end
+  end
+
+  def game_over?
+    if @secret == @guess_array.join("")
+      return true
+    elsif @guesses == 0
+      return true
+    else
+      return false
+    end
   end
 
 end
 
 g = Game.new
 p g.secret
-p g.secret_array
-p g.guess_array.join(" ")
 g.play
